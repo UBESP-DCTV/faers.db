284756206505ec996e81c01e4f3c75f890b60230
Nicola <ni.guidone@studenti.unina.it>
2020-12-26 12:51:22 +0100

    trasform date and integer for read_demo #2

--- R/read_faers.R
+++ R/read_faers.R
@@ -10,9 +10,12 @@
 #'    }
 
 read_demo <- function(path) {
-  x <- utils::read.csv(path, sep = "$")
-  x <- x %>%  dplyr::mutate_at(c("i_f_code", "rept_cod","mfr_sndr", "sex",
-                                 "e_sub", "to_mfr","occp_cod",) , as.factor) #transform to factor
-  # init_fda_dt, fda_dt,repr_dt to date
+  x <- readr::read_delim(path, delim = "$")
+  x <- x %>%  dplyr::mutate(across(c("i_f_code", "rept_cod","mfr_sndr", "sex",
+                                     "e_sub", "to_mfr","occp_cod") , as.factor), #transform to factor
+                            across(c("event_dt","mfr_dt", "init_fda_dt",
+                                     "fda_dt", "rept_dt"  ), ymd) ,
+                            across(c("primaryid", "caseid", "caseversion" ,"age"), as.integer))
   x
 }
+
