6338b304864f9c56feda9f32cfad692f6b57a42a
Nicola <ni.guidone@studenti.unina.it>
2021-01-04 12:46:40 +0100

    added test read_*
    added and recheck variable class

--- tests/testthat/test-read_faers.R
+++ tests/testthat/test-read_faers.R
@@ -39,3 +39,144 @@ test_that("read_demo works", {
   )
 
 })
+
+
+test_that("read drug works",{
+  db_test_path <-  system.file("testing-data/DRUG20Q3-10.txt",
+                               package = "faers.db"
+  )
+
+  res <- read_drug(db_test_path)
+  expect_is(res, "tbl_df")
+
+  expected_class_names <- c( #
+    primaryid = "integer",
+    caseid = "integer",
+    drug_seq = "integer",
+    role_cod = "factor",
+    drugname = "character",
+    prod_ai = "character",
+    val_vbm = "numeric",
+    route = "character",
+    dose_vbm = "character",
+    cum_dose_chr = "numeric",
+    cum_dose_unit = "logical",
+    dechal = "factor",
+    rechal = "factor",
+    lot_num = "logical",
+    exp_dt = "POSIXct",
+    nda_num = "integer",
+    dose_amt = "integer",
+    dose_unit = "character",
+    dose_form = "character",
+    dose_freq = "character"
+  )
+  expect_equal(
+    purrr::map_chr(res, ~class(.x)[[1L]]),
+    expected_class_names
+  )
+})
+
+
+test_that("read indi works",{
+  db_test_path <-  system.file("testing-data/INDI20Q3-10.txt",
+                               package = "faers.db"
+  )
+
+  res <- read_indi(db_test_path)
+  expect_is(res, "tbl_df")
+
+  expected_class_names <- c(
+    primaryid = "integer",
+    caseid = "integer",
+    indi_drug_seq = "integer",
+    indi_pt = "character"
+  )
+  expect_equal(
+    purrr::map_chr(res, ~class(.x)[[1L]]),
+    expected_class_names
+  )
+})
+
+
+
+test_that("read outc works",{
+  db_test_path <-  system.file("testing-data/OUTC20Q3-10.txt",
+                               package = "faers.db"
+  )
+
+  res <- read_outc(db_test_path)
+  expect_is(res, "tbl_df")
+
+  expected_class_names <- c(
+    primaryid = "integer",
+    caseid = "integer",
+    outc_cod = "factor"
+  )
+  expect_equal(
+    purrr::map_chr(res, ~class(.x)[[1L]]),
+    expected_class_names
+  )
+})
+
+test_that("read reac works",{
+  db_test_path <-  system.file("testing-data/REAC20Q3-10.txt",
+                               package = "faers.db"
+  )
+
+  res <- read_reac(db_test_path)
+  expect_is(res, "tbl_df")
+
+  expected_class_names <- c(
+    primaryid = "integer",
+    caseid = "integer",
+    pt = "character",
+    drug_rec_act = "logical"
+  )
+  expect_equal(
+    purrr::map_chr(res, ~class(.x)[[1L]]),
+    expected_class_names
+  )
+})
+
+test_that("read rpsr works",{
+  db_test_path <-  system.file("testing-data/RPSR20Q3-10.txt",
+                               package = "faers.db"
+  )
+
+  res <- read_rpsr(db_test_path)
+  expect_is(res, "tbl_df")
+
+  expected_class_names <- c(
+    primaryid = "integer",
+    caseid = "integer",
+    rpsr_cod = "factor"
+  )
+  expect_equal(
+    purrr::map_chr(res, ~class(.x)[[1L]]),
+    expected_class_names
+  )
+})
+
+test_that("read ther works",{
+  db_test_path <-  system.file("testing-data/THER20Q3-10.txt",
+                               package = "faers.db"
+  )
+
+  res <- read_ther(db_test_path)
+  expect_is(res, "tbl_df")
+
+  expected_class_names <- c(
+    primaryid = "integer",
+    caseid = "integer",
+    dsg_drug_seq = "integer",
+    start_dt = "POSIXct",
+    end_dt = "POSIXct",
+    dur = "numeric",
+    dur_cod = "character"
+  )
+  expect_equal(
+    purrr::map_chr(res, ~class(.x)[[1L]]),
+    expected_class_names
+  )
+})
