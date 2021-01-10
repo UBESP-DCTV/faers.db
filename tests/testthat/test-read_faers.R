2738d06c106e484e1cfa61696e17170b089c0450
Nicola <ni.guidone@studenti.unina.it>
2021-01-10 15:26:15 +0100

    added test date

--- tests/testthat/test-read_faers.R
+++ tests/testthat/test-read_faers.R
@@ -181,3 +181,48 @@ test_that("read ther works", {
     expected_class_names
   )
 })
+
+
+
+#testa se impporta in modo corretto differenti tipi di dati
+test_that("read_demo works", {
+  db_test_path <- system.file("testing-data/DEMO20Q3-10.txt",
+                              package = "faers.db"
+  )
+  res <- read_demo(db_test_path)
+  expect_is(res, "tbl_df")
+
+  expected_class_names <- c(
+    primaryid = "numeric",
+    caseid = "integer",
+    caseversion = "integer",
+    i_f_code = "factor",
+    event_dt = "POSIXct",
+    mfr_dt = "POSIXct",
+    init_fda_dt = "POSIXct",
+    fda_dt = "POSIXct",
+    rept_cod = "factor",
+    auth_num = "character",
+    mfr_num = "character",
+    mfr_sndr = "factor",
+    lit_ref = "character",
+    age = "integer",
+    age_cod = "character",
+    age_grp = "character",
+    sex = "factor",
+    e_sub = "factor",
+    wt = "numeric",
+    wt_cod = "character",
+    rept_dt = "POSIXct",
+    to_mfr = "factor",
+    occp_cod = "factor",
+    reporter_country = "character",
+    occr_country = "character"
+  )
+
+  expect_equal(
+    purrr::map_chr(res, ~class(.x)[[1L]]),
+    expected_class_names
+  )
+
+})
