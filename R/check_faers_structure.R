d5cc46379e518c478c3fbed21a95b0d4a8fb554b
Pietro <pietro.belloni@outlook.it>
2021-01-16 18:29:08 +0100

    check_root_folder function

---
+++ R/check_faers_structure.R
@@ -0,0 +1,14 @@
+check_root_folder <- function(path) {
+  dirlist <- list.dirs(path, recursive = FALSE)
+  is_faers_folder <- grepl("faers_raw_data", dirlist)
+  if (sum(is_faers_folder) == 0) {
+    warning("I can't find a folder named 'faers_raw_data'.")
+    return(FALSE)
+  }
+  if (sum(is_faers_folder) > 1) {
+    warning("Too many folders named 'faers_raw_data' (or similar).
+            Please keep only one.")
+    return(FALSE)
+  }
+  TRUE
+}
---
+++ tests/testthat/test-check_faers_structure.R
@@ -0,0 +1,3 @@
+test_that("check_root_folder works", {
+
+})
