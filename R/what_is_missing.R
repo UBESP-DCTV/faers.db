6c205aa138d07e067811cfe99173cb4e46a6bf03
Pietro <pietro.belloni@outlook.it>
2021-02-19 09:52:01 +0100

    check ok

--- R/what_is_missing.R
+++ R/what_is_missing.R
@@ -21,10 +21,11 @@ what_is_missing <- function(path, faers_meta = fetch_faers_meta()) {
   local <- prepare_local_meta(path)
   online <- prepare_online_meta(faers_meta)
   out <- online %>%
-    dplyr::filter(online[["unq"]] %in% local[["unq"]] == FALSE) %>%
-    dplyr::select(-unq) %>%
-    replace(is.na(.), 0L) %>%
-    dplyr::transmute(year, quarter, type, mb = ascii_zip_mb + xml_zip_mb)
+    dplyr::filter(!.data[["unq"]] %in% local[["unq"]]) %>%
+    dplyr::select(-.data[["unq"]]) %>%
+    replace(is.na(.data[["."]]), 0L) %>%
+    dplyr::transmute(.data[["year"]], .data[["quarter"]], .data[["type"]],
+                     mb = .data[["ascii_zip_mb"]] + .data[["xml_zip_mb"]])
   totmb <- sum(out[["mb"]])
   message(glue::glue("{NROW(out)} FAERS databases are missing in your folder",
                      " ({totmb} mb)."))
@@ -34,8 +35,10 @@ what_is_missing <- function(path, faers_meta = fetch_faers_meta()) {
 
 prepare_local_meta <- function(path) {
   fetch_local(path) %>%
-    subset(select = c(year, quarter, type)) %>%
-    tidyr::unite(unq, c(year, quarter, type), remove = FALSE)
+    subset(select = c(.data[["year"]], .data[["quarter"]], .data[["type"]])) %>%
+    tidyr::unite(.data[["unq"]],
+                 c(.data[["year"]], .data[["quarter"]], .data[["type"]]),
+                 remove = FALSE)
 }
 
 
@@ -43,12 +46,20 @@ prepare_online_meta <- function(faers_meta) {
   n <- NROW(faers_meta)
   dplyr::bind_rows(
     faers_meta %>%
-      subset(select = c(year, quarter, ascii_zip_mb)) %>%
+      subset(select = c(.data[["year"]],
+                        .data[["quarter"]],
+                        .data[["ascii_zip_mb"]])) %>%
       tibble::add_column(type = rep("ascii", n)) %>%
-      tidyr::unite(unq, c(year, quarter, type), remove = FALSE),
+      tidyr::unite(.data[["unq"]],
+                   c(.data[["year"]], .data[["quarter"]], .data[["type"]]),
+                   remove = FALSE),
     faers_meta %>%
-      subset(select = c(year, quarter, xml_zip_mb)) %>%
+      subset(select = c(.data[["year"]],
+                        .data[["quarter"]],
+                        .data[["xml_zip_mb"]])) %>%
       tibble::add_column(type = rep("xml", n)) %>%
-      tidyr::unite(unq, c(year, quarter, type), remove = FALSE)
+      tidyr::unite(.data[["unq"]],
+                   c(.data[["year"]], .data[["quarter"]], .data[["type"]]),
+                   remove = FALSE)
   )
 }
